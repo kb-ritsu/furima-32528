9a1477fe14064b3e459004750e9f95d6f9de22c0
kb-ritsu <microyasuki@hotmail.co.jp>
2020-12-16 14:21:10 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
     
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.create(item_params)
+   
+    if  @item.save
+      redirect_to "/"
+    else      
+      redirect_to action: :new
+    end
+  end
+  private
+
+  def  item_params 
+    params.require(:item).permit(:name, :price, :category_id, :status_id,:fee_id,:place_id,:user,:detail,:day_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :users_item
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :place
+  belongs_to :day
 
-  validates :name
-  validates
+  validates :name, :price, :category_id, :status_id, :fee_id, :place_id, :user, :detail, :day_id, presence: true
+  validates :category_id,:status_id,:fee_id,:place_id,:day_id,  numericality: { other_than: 0 }
 end
