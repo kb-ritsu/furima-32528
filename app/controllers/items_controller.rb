50aa7a491f91cf160022cdc9ee2d99565fbe3099
kb-ritsu <microyasuki@hotmail.co.jp>
2020-12-16 18:08:49 +0900

    値段の計算機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,17 +8,16 @@ class ItemsController < ApplicationController
   end
   def create
     @item = Item.create(item_params)
-   
     if  @item.save
       redirect_to "/"
     else      
-      redirect_to action: :new
+      render :new
     end
   end
   private
 
   def  item_params 
-    params.require(:item).permit(:name, :price, :category_id, :status_id,:fee_id,:place_id,:user,:detail,:day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :category_id, :status_id,:fee_id,:place_id,:user,:detail,:day_id, :image).merge(user_id: current_user.id)
   end
 
 end
