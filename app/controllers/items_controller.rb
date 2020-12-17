726d0358b975938cedc2342b4633725f7c66443f
kb-ritsu <microyasuki@hotmail.co.jp>
2020-12-17 18:04:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
     end
   end
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
