2f2a048473645158c5412127cfc714e9b10c9433
kb-ritsu <microyasuki@hotmail.co.jp>
2020-12-22 11:00:38 +0900

    Revert "Revert "購入機能の実装""
    This reverts commit ac0dec59e1e2bf1d30045fd75618d71114101a7e.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   resources :users
-  resources :items
+  resources :items do
+    resources :orders, only: [:create, :index, :new]
+  end
 end
